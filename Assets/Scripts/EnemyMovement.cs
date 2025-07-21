c6e3a882bfa007566d0f26fd69be3d269c88471a
VRiek <riekkiv@gmail.com>
2025-07-21 10:16:49 +0300

    Enemy added

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform player;
+    private NavMeshAgent navMeshAgent;
+
+    public enum StateMachine
+    {
+        Idle,
+        Engage
+    }
+
+    public StateMachine currentState;
+    public float speed = 3f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case StateMachine.Idle:
+                Idle();
+                break;
+            case StateMachine.Engage:
+                Engage();
+                break;
+        }
+
+        bool playerSeen = Vector3.Distance(transform.position, player.position) < 10f;
+
+        if (!playerSeen && currentState != StateMachine.Idle)
+        {
+            currentState = StateMachine.Idle;
+        }
+        else if (playerSeen && currentState != StateMachine.Engage)
+        {
+            currentState = StateMachine.Engage;
+        }
+    }
+
+    void Idle()
+    {
+        Debug.Log("Enemy is idling.");
+        return;
+    }
+
+    void Engage()
+    {
+        Debug.Log("Enemy is engaging.");
+        navMeshAgent.SetDestination(player.position);
+    }
+}
